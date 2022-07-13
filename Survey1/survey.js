00b5e982b9834fec85fbc0a238d2c10da4764b6c
gagandeepkatodia <gagandeepkatodia@gmail.com>
2022-07-13 15:23:02 +0200

    first survey code after designing

--- Survey1/survey.js
+++ Survey1/survey.js
@@ -1,5 +1,9 @@
-console.log("hello from JS");
-
-
-
-function 
+var imgCount = 1;
+var images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
+function changeImage() {
+  if (imgCount !== images.length - 1) imgCount++;
+  else imgCount = 0;
+  var image = document.getElementById("image1");
+  image.src = images[imgCount];
+}
+//export default changeImage;
